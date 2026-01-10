e655dab4c7d43bb81f7d0f3bbdb1acaf310f80b5
Aryan <aryan.sharma@intimetec.com>
2026-01-10 12:37:09 +0530

    Fourth Question Completed

--- Q4.py
+++
@@ -1,8 +0,0 @@
-size = int(input("Enter size of pyramid required: "))
-
-for i in range (0 , size):
-    for j in range (0 , size - i - 1):
-        print(" " , end = "")
-    for k in range(0 , 2 * i + 1):
-        print("*" , end = "")
-    print("")
